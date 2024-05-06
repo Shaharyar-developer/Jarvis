d92ca9ae6ed8722e1b7c65bb4b44ec34b78af33d
shaharyar <shaharyar321321@gmail.com>
2024-05-06 21:16:25 +0500

    and typesafe env implementation

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,6 +2,12 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {},
-  runtimeEnv: {},
+  server: {
+    OPENAI_API_KEY: z.string(),
+    ELEVENLABS_API_KEY: z.string(),
+  },
+  runtimeEnv: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY,
+  },
 });
