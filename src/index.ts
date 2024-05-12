b64b1c9d75d78a80069afa329f45e64105db71da
shaharyar <shaharyar321321@gmail.com>
2024-05-12 19:47:18 +0500

    Add comments to index file for better code structure understanding

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+/**
+ * This is the main entry point of the application.
+ * This is how it goes:
+ *  Chat loop is an async generator
+ *  When ran:
+ *  1. It creates or gets an assistant, if create then saves its id to redis db
+ *  2. It creates or gets a thread if create then saves its id to redis db
+ *  3. It starts a while loop
+ *  4. It runs a function that uses a custom VAD algorithm from shaharyar-dev
+ *  5. When speech is stopped it saves the entire audio file to an output.wav
+ *  6. It gets a transcription from openai whisper and then adds that     transcription to the thread
+ *  7. It creates a run stream and starts a for loop on the events
+ *  8. It checks the event.event element of the object to determine what to do, e.g; extracting text or submitting tool outputs
+ *  9. It yields the message to the chat loop
+ *  10. The message is then passed to the say function which is a wrapper function around the elevenlabs library that takes text, generates audio and then plays it with ffmpeg and alsa
+ */
+
 import { chatLoop } from "@/openai/assistants/main";
 import { say } from "@/elevenlabs/generate";
 
