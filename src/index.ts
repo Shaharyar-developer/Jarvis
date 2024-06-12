e944b1f7c54e80da286d740fb6526863bbae17cb
shaharyar <shaharyar321321@gmail.com>
2024-06-13 02:37:55 +0500

    temp

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,11 @@
 
 import { say } from "@/elevenlabs/generate";
 import { chatLoop } from "@/openai/assistants/main";
+import { deleteRun } from "./openai/assistants/helpers";
+try {
+  await deleteRun();
+} catch (error) {}
+  
 
 for await (const message of chatLoop()) {
   await say(message);
