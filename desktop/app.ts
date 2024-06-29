c7d71a33b8b3f3e761232b4225c29dada5310cd0
Shaharyar <shaharyar321321@gmail.com>
2024-06-29 22:49:16 +0500

    REINIT PROJECT

--- desktop/app.ts
+++
@@ -1,6 +0,0 @@
-import { Webview } from "webview-bun";
-const ww = new Webview();
-
-ww.navigate("http://localhost:5173/");
-ww.title = "Nyx";
-ww.run();
--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
-export const checkSpeech = async () => {
-  const req = await fetch("http://127.0.0.1:3000");
-  const res = (await req.json()) as { speech: boolean };
-  return res;
-};
